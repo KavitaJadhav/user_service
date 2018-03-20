16009ee47db56581bcc2919ede6bbbbe753a60c5
Kavita Jadhav <jadhavkavita206@gmail.com>
2018-03-20 21:50:02 +0530

    Added letter_opener gem to open email in browser in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -51,4 +52,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Sends mail in browser
+  ActionMailer::Base.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host =>  'localhost:3000'}
+  config.action_mailer.delivery_method = :letter_opener
+
 end
