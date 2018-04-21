0422141bd01b0fec362d0b8e70e9f9a23dbe10c3
Kavita Jadhav <jadhavkavita206@gmail.com>
2018-04-21 12:31:13 +0530

    Added route to see sidekiq dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users
+mount Sidekiq::Web => '/sidekiq'
 end
