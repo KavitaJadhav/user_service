95863bafbd3b75247f2b3e1eeb223a9f3a82db0f
Kavita Jadhav <jadhavkavita206@gmail.com>
2018-04-21 12:40:58 +0530

    Disabled retry failed job for number_printer_worker.

--- app/workers/number_printer_worker.rb
+++ app/workers/number_printer_worker.rb
@@ -1,6 +1,6 @@
 class NumberPrinterWorker
   include Sidekiq::Worker
-  sidekiq_options queue: 'priority'
+  sidekiq_options queue: 'priority', :retry => false
 
   def perform(*args)
     for i in 1..100
