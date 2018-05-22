abd53f4fd8707c5605b326613588cfcbed8dc944
Kavita Jadhav <jadhavkavita206@gmail.com>
2018-05-22 23:11:55 +0530

    Using custom exporter to add status of dependencies.

--- config.ru
+++ config.ru
@@ -1,8 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-require 'prometheus/middleware/exporter'
 
-use Prometheus::Middleware::Exporter
+use Prometheus::Middleware::CustomExporter
 
 run Rails.application
--- config/initializers/custom_exporter.rb
+++ config/initializers/custom_exporter.rb
@@ -1,10 +1,34 @@
 require 'prometheus/middleware/exporter'
+require 'net/http'
 
 module Prometheus
   module Middleware
     class CustomExporter < Prometheus::Middleware::Exporter
       def respond_with(format)
+        byebug
+        guage = @registry.metrics.first
+        guage.set({name: :customerservice, env: Rails.env, description: customer_service_url}, customer_service_up?)
+
         super
+      end
+
+      private
+
+      def customer_service_up?
+        begin
+          url = URI.parse(customer_service_url)
+          req = Net::HTTP::Get.new(url.to_s)
+          res = Net::HTTP.start(url.host, url.port) { |http|
+            http.request(req)
+          }
+          return 1
+        rescue
+          return 0
+        end
+      end
+
+      def customer_service_url
+        'http://localhost:3001'
       end
     end
   end
